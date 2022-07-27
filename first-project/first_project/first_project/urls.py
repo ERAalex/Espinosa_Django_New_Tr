f02d1538b54d6401ac950a257b0c16eb249a9f5b
Alex_ERA <eraspb@mail.ru>
2022-07-27 09:38:14 +0300

    27.07.2022 changes

--- first-project/first_project/first_project/urls.py
+++ first-project/first_project/first_project/urls.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from django.urls import path
 from app.views import home_view, time_view, workdir_view
 
+
 urlpatterns = [
     path('', home_view, name='home'),
     path('current_time/', time_view, name='time'),
     path('workdir/', workdir_view, name='workdir'),
     path('admin/', admin.site.urls),
+
 ]
