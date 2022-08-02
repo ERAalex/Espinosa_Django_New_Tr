55574c9ad00f6c6e56524df6cbd89c280d97d2ac
Alex_ERA <eraspb@mail.ru>
2022-08-02 09:41:48 +0300

    New changes working on cvs file in import_phones.py script

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -3,10 +3,10 @@ from datetime import date
 
 
 class Phone(models.Model):
-    id =models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=60, blank=True)
-    price =  models.CharField(max_length=60, blank=True)
     image = models.ImageField(upload_to='static/img', blank=True)
+    price = models.CharField(max_length=60, blank=True)
     release_date = models.DateField(default=date.today)
     lte_exists = models.BooleanField(default=False)
     slug = models.SlugField(unique=True, blank=True)
