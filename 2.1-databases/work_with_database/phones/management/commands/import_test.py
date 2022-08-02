082d5bb27a5692307ce423b645ecc5b93de57d6e
Alex_ERA <eraspb@mail.ru>
2022-08-02 23:26:00 +0300

    All working :D csv file -> models

--- 2.1-databases/work_with_database/phones/management/commands/import_test.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_test.py
@@ -1,7 +1,5 @@
-
 import csv
 from django.core.management.base import BaseCommand
-from phones.models import Phone
 
 
 class Command(BaseCommand):
@@ -11,7 +9,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         with open('phones.csv', 'r') as file:
             phones = list(csv.DictReader(file, delimiter=';'))
-        for phone in phones:
-            data = Phone(id=phone['id'], name=phone['name'], image=phone['image'], price=phone['price'], release_date=phone['release_date'], lte_exists=phone['lte_exists'])
-            data.save()
+            for row in phones:
+                print(row)
+
 
