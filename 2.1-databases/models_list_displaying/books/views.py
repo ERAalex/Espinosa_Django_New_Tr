ef08ab629dc3cb9645bcb23e9a5c49c720522e39
Alex_ERA <eraspb@mail.ru>
2022-08-04 01:05:08 +0300

    new work at library online

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
+from books.models import Book
 
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
-    return render(request, template, context)
+    context = Book.objects.all()
+    return render(request, template, {'context': context})
+
+
+def sort_name(request):
+    template = 'books/books_list.html'
+    context = Book.objects.filter().order_by('name')
+    return render(request, template, {'context' : context})
+
+def sort_date_old(request):
+    template = 'books/books_list.html'
+    context = Book.objects.filter().order_by('pub_date')
+    return render(request, template, {'context' : context})
+
+def sort_date_new(request):
+    template = 'books/books_list.html'
+    context = Book.objects.filter().order_by('pub_date').reverse()
+    return render(request, template, {'context' : context})
+
+
+
--- 2.1-databases/models_list_displaying/main/urls.py
+++ 2.1-databases/models_list_displaying/main/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from books.views import books_view
+from books.views import books_view, sort_date_old, sort_date_new, sort_name
 
 
 
@@ -8,4 +8,7 @@ from books.views import books_view
 urlpatterns = [
     path('', books_view, name='books'),
     path('admin/', admin.site.urls),
+    path('books_name', sort_name, name='sort_name'),
+    path('books_old', sort_date_new, name='sort_date_old'),
+    path('books_new', sort_date_old, name='sort_date_new'),
 ]
