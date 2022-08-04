fa3d0abf39284599b83ba2bda3aedd6709e3199e
Alex_ERA <eraspb@mail.ru>
2022-08-04 12:30:43 +0300

    added the field slug in models

--- 2.1-databases/models_list_displaying/books/models.py
+++ 2.1-databases/models_list_displaying/books/models.py
@@ -7,6 +7,12 @@ class Book(models.Model):
     name = models.CharField(u'Название', max_length=64)
     author = models.CharField(u'Автор', max_length=64)
     pub_date = models.DateField(u'Дата публикации')
+    slug = models.SlugField(blank=True)
+
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Book, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name + " " + self.author
