8a3a2294f266e98eeeaa88fb1b74815740cac5ff
Alex_ERA <eraspb@mail.ru>
2022-08-09 17:07:45 +0300

    task1 done

--- 2.2-databases-2/orm_migrations/school/models.py
+++ 2.2-databases-2/orm_migrations/school/models.py
@@ -15,7 +15,7 @@ class Teacher(models.Model):
 
 class Student(models.Model):
     name = models.CharField(max_length=30, verbose_name='Имя')
-    teacher = models.ManyToManyField(Teacher, related_name='students')
+    teacher = models.ManyToManyField(Teacher, related_name='teachers')
     group = models.CharField(max_length=10, verbose_name='Класс')
 
     class Meta:
@@ -24,3 +24,5 @@ class Student(models.Model):
 
     def __str__(self):
         return self.name
+
+
--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -1,13 +1,13 @@
 from django.views.generic import ListView
 from django.shortcuts import render
-
-from .models import Student
+from .models import Teacher, Student
 
 
 def students_list(request):
     template = 'school/students_list.html'
-
-    context = {}
+    student = Student.objects.all()
+    teacher = Teacher.objects.all()
+    context = {'student': student, 'teacher': teacher}
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
